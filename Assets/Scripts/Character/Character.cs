31910e7e0708f3b5bec62a1df1a9b7075e113f6c
MotoTK <mototk@sbcglobal.net>
2017-03-13 23:09:25 -0700

    Refactored character movement to new file CharacterMovement. Set up CharacterHealth.cs file with add/remove health public methods. Stubbed CharacterCombat file.

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -7,15 +7,7 @@ using Random = UnityEngine.Random;
 
 public class Character : MonoBehaviour{
 
-	public bool userInputBool;
-	public float horizSpeed;
-	public float maxCharacterHorizVelocity;
-
-	public int numOfJumps;
-	public float vertJumpSpeed;
-	private int curJumpsLeft;
-	private float prevVertInput;
-	private bool hasPhysicsJump;
+	private CharacterMovement characterMovement;
 
 	private Rigidbody2D rb2d;
 	private Animator animator;
@@ -26,17 +18,21 @@ public class Character : MonoBehaviour{
 	private float vertInput = 0;
 
 	private void Start () {
+		characterMovement = GetComponent<CharacterMovement> ();
 		animator = GetComponent<Animator>();
 		boxCollider = GetComponent<BoxCollider2D> ();
-		rb2d = gameObject.GetComponent<Rigidbody2D> ();
 	}
 
 	private void Update () {
-		handleUserInputLogic ();
+		
+	}
+
+	void FixedUpdate(){
+
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
-		managePlatformLogic (other.gameObject);
+		
 	}
 
 	void OnCollisionExit2D(Collision2D other){
@@ -49,48 +45,5 @@ public class Character : MonoBehaviour{
 
 	void OnTriggerExit2D(Collider2D other){
 		
-	}
-
-	void FixedUpdate(){
-		float vertForce = getVertForce(rb2d);
-		float horizForce = getHorizForce();
-
-		rb2d.AddForce (new Vector2 (horizForce, vertForce));
-		rb2d.velocity = new Vector2 (Mathf.Clamp (rb2d.velocity.x, -maxCharacterHorizVelocity, maxCharacterHorizVelocity), rb2d.velocity.y);
-	}
-
-	private void handleUserInputLogic(){
-		if (userInputBool) {
-			horizInput = Input.GetAxisRaw ("Horizontal");
-			vertInput = Input.GetAxisRaw ("Vertical");
-			updateJump (vertInput);
-		}
-	}
-
-	private void managePlatformLogic(GameObject gameObject){
-		if (gameObject.tag == "Platform") {
-			curJumpsLeft = numOfJumps;
-		}
-	}
-	private void updateJump (float curVertInput){
-		if (prevVertInput == 0 && curVertInput > 0) {
-			if (curJumpsLeft > 0) {
-				hasPhysicsJump = true;
-			}
-			curJumpsLeft--;
-		}
-		prevVertInput = curVertInput;
-	}
-
-	private float getVertForce(Rigidbody2D rgbd){
-		if (hasPhysicsJump) {
-			hasPhysicsJump = false;
-			rgbd.velocity = new Vector2 (rb2d.velocity.x, 0);
-			return (vertJumpSpeed * vertInput);
-		}
-		return 0;
-	}
-	private float getHorizForce(){
-		return horizSpeed * horizInput;
 	}
 }
---
+++ Assets/Scripts/Character/CharacterHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour {
+
+	public float initialHealth;
+	private float curHealth;
+	// Use this for initialization
+	void Start () {
+		curHealth = initialHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Will return true if inclicted damage killed character
+	public bool inflictDamage(float damage){
+		curHealth -= damage;
+		return isCharacterDead ();
+	}
+	private bool isCharacterDead(){
+		if (curHealth < 0) {
+			return true;
+		}
+		return false;
+	}
+	public void addHealth(float hp){
+		curHealth += hp;
+	}
+}
