9deaa88a9078384192878ac7813c68bd8cd0b002
Thomas Trzcinski <trzcinski.tommy@gmail.com>
2017-05-11 12:32:42 -0700

    Added scripts to put values to dropdowns and new scenes for vsai and local mp

---
+++ Assets/Scripts/DropdownPlayer1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class DropdownPlayer1 : MonoBehaviour
+{
+    List<string> fighters = new List<string>() { "Please Select Player1 Fighter", "P1 Fire", "P1 Air", "P1 Earth", "P1 Water" };
+    public Dropdown dropdown;
+    public Text selectedFighter;
+
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        selectedFighter.text = fighters[index];
+        if (index == 0)
+        {
+            selectedFighter.color = Color.red;
+        }
+        else
+        {
+            selectedFighter.color = Color.white;
+        }
+    }
+
+
+    void Start()
+    {
+        PopulateList();
+    }
+
+    void PopulateList()
+    {
+
+        dropdown.AddOptions(fighters);
+    }
+
+
+
+}
---
+++ Assets/Scripts/DropdownStage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+
+public class DropdownStage : MonoBehaviour
+{
+    List<string> stages = new List<string>() { "Please Select a Stage", "Battleground" };
+    public Dropdown dropdown;
+    public Text selectedStage;
+
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        selectedStage.text = stages[index];
+        if (index == 0)
+        {
+            selectedStage.color = Color.red;
+        }
+        else
+        {
+            selectedStage.color = Color.white;
+        }
+    }
+
+
+    void Start()
+    {
+        PopulateList();
+    }
+
+    void PopulateList()
+    {
+
+        dropdown.AddOptions(stages);
+    }
+
+
+
+}
