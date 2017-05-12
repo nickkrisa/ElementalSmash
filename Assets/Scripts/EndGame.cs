4c3671300fa0d8a4c4c61369b73ab001cd1a7489
Tommy Trzcinski <Tommy Trzcinski>
2017-05-11 17:49:58 -0700

    Endgame now declares the winner.

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndGame : MonoBehaviour {
 
+    public Text text;
+    private int playerdead;
+
 	// Use this for initialization
 	void Start () {
-		
+        playerdead = PlayerPrefs.GetInt("DeadGuy");
+
+        if (playerdead == PlayerPrefs.GetInt("FighterType1"))
+        {
+            text.text = "Player 2 Wins!";
+
+        }
+        else if (playerdead == PlayerPrefs.GetInt("FighterType2"))
+        {
+            text.text = "Player 1 Wins!";
+        }
 	}
 	
 	// Update is called once per frame
